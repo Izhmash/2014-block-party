c90ab393f690c2ccd81c20813fe4bb9d6a87ab06
FTC-3397 <ianballou67@gmail.com>
2013-12-21 14:12:29 -0500

    testing

--- Testing/motortest.c
+++ Testing/motortest.c
@@ -25,10 +25,10 @@ task main()
 	getJoystickSettings(joystick);
 	while(true)
 	{
-		motor[LBoomMotor] = joystick.joy1_y1 / factor;
-		motor[RBoomMotor] = joystick.joy1_y2 / factor;
-
-		//test
+		//motor[LBoomMotor] = joystick.joy1_y1 / factor;
+		writeDebugStreamLine("%f", joystick.joy1_y1 / factor);
+		//motor[RBoomMotor] = joystick.joy1_y2 / factor;
+		writeDebugStreamLine("%f", joystick.joy1_y2 / factor);
 	}
 
 }
