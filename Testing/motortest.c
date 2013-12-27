b80ebb2d3a866c1bf16e90384c792dee3ed56166
FTC-3397 <ianballou67@gmail.com>
2013-12-27 16:15:05 -0500

    changed for new setup

--- Testing/motortest.c
+++ Testing/motortest.c
@@ -22,16 +22,18 @@
 
 #include "joystickdriver.c"
 
-//float factor = .63;
-float factor = .1969;
+//float factor = -.63;
+//float factor = -.1969;
+float factor = -1.27;
+
 
 task main()
 {
 	getJoystickSettings(joystick);
 	while(true)
 	{
-	motor[LBoomMotor] = joystick.joy1_y1 * factor;
-	motor[RBoomMotor] = joystick.joy1_y1 * factor;
+	motor[LBoomMotor] = joystick.joy1_y1 / factor;
+	motor[RBoomMotor] = joystick.joy1_y1 / factor;
 	//writeDebugStreamLine("%f", joystick.joy1_y1 / factor);
 	//motor[RBoomMotor] = joystick.joy1_y2 / factor;
 	//writeDebugStreamLine("%f", joystick.joy1_y2 / factor);
