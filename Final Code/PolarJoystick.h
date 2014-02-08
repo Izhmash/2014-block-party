28acd7fb99bf429e0704ec0016d74abcb25deba9
FTC-3397 <ianballou67@gmail.com>
2014-02-08 15:12:39 -0500

    Conversion to Polar Coordinate JStick

---
+++ Final Code/PolarJoystick.h
@@ -0,0 +1,14 @@
+float getMagnitude(float X, float Y)
+{
+	return sqrt(pow(X, 2) + pow(Y, 2));
+}
+
+float getDirRads(float X, float Y)
+{
+	return atan2(Y, X);
+}
+
+float getDirDegrees(float X, float Y)
+{
+	return (180/acos(-1))*getDirRads(X, Y);
+}
